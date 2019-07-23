bebac9014978af60157594a9dd6aba31ca948945
Jason Liang <jasonliang@Jasons-MacBook-Pro-2.local>
2019-07-23 14:47:56 -0700

    s is the thing

---
+++ hackathon_/videoparseWindow.py
@@ -0,0 +1,37 @@
+'''
+video parsing for PCs with Windows operation system.
+Purpose: 
+    video is parsed to frames and stored into a folder called 'data'
+'''
+
+
+import cv2
+import numpy as np
+import os
+import time
+
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print ('Error: Creating directory of data')
+
+
+start = time.time()
+start2 = start + 3
+currentFrame = 0
+while time.time() <= start2:
+    cap = cv2.VideoCapture(0) 
+    ret, frame = cap.read()
+    if ret == False: 
+        break
+    name = './data/frame' + str(currentFrame) + '.png'
+    print ('Creating...' + name)
+    cv2.imwrite(name, frame)
+    
+    currentFrame += 1
+    
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
