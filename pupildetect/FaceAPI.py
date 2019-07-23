e36b2c3086ec20d3d76f1d9fe0b40ad8ac9516be
Mischa Johal <MischaJohal@Mischas-MacBook-Pro.local>
2019-07-23 11:26:46 -0700

    commit

--- pupildetect/FaceAPI.py
+++ pupildetect/FaceAPI.py
@@ -5,8 +5,7 @@ assert subscription_key
 
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-image_url = 'https://cdn.discordapp.com/attachments/413154933278507008/603272125884923947/20190723_100739.jpg', 'https://media.discordapp.net/attachments/413154933278507008/603272127176507392/20190723_100743.jpg?width=681&height=908'
-
+image_url = 'https://media.discordapp.net/attachments/413154933278507008/603272127176507392/20190723_100743.jpg?width=681&height=908'
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
 params = { 
@@ -25,6 +24,7 @@ data = response.json()
 
 face_data = data[0]
 faceLandmarks = (face_data['faceLandmarks'])
+
 #print (faceLandmarks)
 
 pupilLeft = faceLandmarks['pupilLeft']
