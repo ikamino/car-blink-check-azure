cb4ccaead61a998ed9252db07562a74554cff9a2
Mischa Johal <MischaJohal@Mischas-MacBook-Pro.local>
2019-07-23 11:59:07 -0700

    help me

--- pupildetect/FaceAPI.py
+++ pupildetect/FaceAPI.py
@@ -5,7 +5,7 @@ assert subscription_key
 
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 
-image_url = 'https://media.discordapp.net/attachments/413154933278507008/603272127176507392/20190723_100743.jpg?width=681&height=908'
+images = ['https://cdn.discordapp.com/attachments/413154933278507008/603272125884923947/20190723_100739.jpg','https://media.discordapp.net/attachments/413154933278507008/603272127176507392/20190723_100743.jpg?width=681&height=908', 'https://cdn.discordapp.com/attachments/413154933278507008/603298808495079424/20190723_114654.jpg', 'https://cdn.discordapp.com/attachments/413154933278507008/603298809493192776/20190723_114701_HDR.jpg']
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
 params = { 
@@ -14,21 +14,24 @@ params = {
 }
 
 
+for image in images:
 
-response = requests.post(face_api_url, params=params,
-                         headers=headers, json={"url": image_url})
+    response = requests.post(face_api_url, params=params,
+                            headers=headers, json={"url": image})
 
 
 
-data = response.json()
+    data = response.json()
 
-face_data = data[0]
-faceLandmarks = (face_data['faceLandmarks'])
+    face_data = data[0]
+    faceLandmarks = (face_data['faceLandmarks'])
 
-#print (faceLandmarks)
+    print (faceLandmarks)
+    pupilLeft = faceLandmarks['pupilLeft']
+    pupilRight = faceLandmarks['pupilRight']
 
-pupilLeft = faceLandmarks['pupilLeft']
-pupilRight = faceLandmarks['pupilRight']
-print ("Pupil Left:", pupilLeft)
-print ("Pupil Right:", pupilRight)
+
+
+    print ("Pupil Left:", pupilLeft)
+    print ("Pupil Right:", pupilRight)
 
