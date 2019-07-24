e591ebc12de5ef4d936f4eca6dda4925ee34251d
Jenny Wei <1155411@learn.vsb.bc.ca>
2019-07-24 18:51:16 +0000

    Updated videoparse.py

--- Video Parse/videoparse.py
+++ Video Parse/videoparse.py
@@ -2,33 +2,33 @@
 '''
 real time record video off of camera then parse video into individual frames
 '''
+
+'''
+real time record video off of camera then parse video into individual frames
+'''
 import cv2
 import numpy as np
 import os
 import time
-
 try:
     if not os.path.exists('data'):
         os.makedirs('data')
 except OSError:
     print ('Error: Creating directory of data')
+    
+#determine whether to use front or back camera
+os_type = int (input('press 0 if you are using an iphone/mac and 1 if you are usign an android phone/windows PC '))
 
-
-start = time.time()
-start2 = start + 60
+start = time.time() + 3
 currentFrame = 0
-while time.time() <= start2:
-    cap = cv2.VideoCapture(0) 
+cap = cv2.VideoCapture(os_type) 
+while time.time() <= start:
     ret, frame = cap.read()
-    if ret == False: 
-        break
     name = './data/frame' + str(currentFrame) + '.png'
     print ('Creating...' + name)
     cv2.imwrite(name, frame)
     
     currentFrame += 1
-    
-
 # When everything done, release the capture
-cap.release()
 cv2.destroyAllWindows()
+cap.release()
